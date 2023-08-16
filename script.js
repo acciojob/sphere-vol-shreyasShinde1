a858bbf34d6573fc1f33d0bba54348fe982400bc
Shreyas Shinde <130788791+shreyasShinde1@users.noreply.github.com>
2023-08-16 20:58:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function volume_sphere() {
     //Write your code here
+	const radius = document.getElementById("radius").value;
+	const volume = document.getElementById("volume");
+
+	let r = radius*radius*radius;
+
+	let v = 4/3 * 3.14 * r;
+	v = Math.ceil(v)
+	volume.value = v;
+	return false;
   
 } 
 
